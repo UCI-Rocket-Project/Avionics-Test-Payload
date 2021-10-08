cd990ba452957725f69adbfb1a7da59b5235247a
Tyler Cook <tylercook017@gmail.com>
2021-10-08 15:28:02 -0700

    Simplification using built-in functions, and receiving header and footer.

--- GroundStation/GroundStation.ino
+++ GroundStation/GroundStation.ino
@@ -1,6 +1,15 @@
+#define HEADER      "Hey,"
+#define TERMINATOR  ",v"
+
+const char START_BYTE = HEADER[0];
+const unsigned int HEADER_LEN = strlen(HEADER);
+const unsigned char maxSentenceSize = 255;
+
+// Packet buffer info/diagnostics variables.
+char sentence[maxSentenceSize+1];
 unsigned int toRead = 0;
-const unsigned char maxSentenceSize = 160;
-char sentence[maxSentenceSize];
+unsigned int alReady = 0;
+
 
 void setup() {
   Serial.begin(115200);
@@ -10,18 +19,48 @@ void setup() {
   Serial1.setRX(0);
   Serial1.setTX(1);
   Serial1.begin(115200);
+
+  sentence[maxSentenceSize] = '\0';
 }
 
 void loop() {
-  toRead = Serial1.available();
+  // Get time for tracking packet rate.
+  
+  
+  // Reset buffer/tracking for this sentence.
+  unsigned int alReady = 0;
+  memset(sentence, 0, maxSentenceSize);
+  unsigned int readThisIter;
 
-  if (toRead > 0) {
-    // XBee has received data
-    Serial1.readBytes(sentence, toRead);
-    Serial.println(sentence);
+  // Receive header.
+  while (!Serial1.findUntil(HEADER, '\0')) {
   }
-  else {
-    // Nothing received.
-    
+
+  // Receive body.
+  Serial1.readBytesUntil(TERMINATOR[1], sentence, maxSentenceSize);
+  Serial.println(sentence);
+  /*
+  while (true) {
+    readThisIter = 0;
+    toRead = Serial1.available();
+    if (toRead > 0) {
+      if ((toRead + alReady) > maxSentenceSize) {
+        break;  
+      }
+      readThisIter = Serial1.readBytesUntil(TERMINATOR[1], sentence+alReady, toRead);
+      alReady += readThisIter;
+      
+      if (readThisIter < toRead && Serial1.peek() == TERMINATOR[1]) {
+        // Publish complete sentence/packet.
+        Serial1.readBytes(sentence+alReady, 1);
+        Serial.println(sentence);
+        break;
+      }
+    }
+    else {
+      // Serial.println("To read: " + String(toRead));
+      // delay(50);
+    }
   }
+  */
 }
