43e811d7d2b71fce3dfebc977011f53b14f16d09
Tyler Cook <tylercook017@gmail.com>
2021-10-11 16:42:15 -0700

    Simplified ground station.

--- GroundStation/GroundStation.ino
+++ GroundStation/GroundStation.ino
@@ -1,15 +1,15 @@
 #define HEADER      "Hey,"
 #define TERMINATOR  ",v"
 
-const char START_BYTE = HEADER[0];
-const unsigned int HEADER_LEN = strlen(HEADER);
 const unsigned char maxSentenceSize = 255;
 
-// Packet buffer info/diagnostics variables.
+// Packet buffer variables.
 char sentence[maxSentenceSize+1];
-unsigned int toRead = 0;
-unsigned int alReady = 0;
 
+// Packet diagnostic variables.
+unsigned int pps;
+unsigned int packetsReceived;
+unsigned int startTime;
 
 void setup() {
   Serial.begin(115200);
@@ -21,16 +21,14 @@ void setup() {
   Serial1.begin(115200);
 
   sentence[maxSentenceSize] = '\0';
+  pps = 0;
+  packetsReceived = 0;
+  startTime = millis();
 }
 
 void loop() {
-  // Get time for tracking packet rate.
-  
-  
-  // Reset buffer/tracking for this sentence.
-  unsigned int alReady = 0;
+  // Reset buffer.
   memset(sentence, 0, maxSentenceSize);
-  unsigned int readThisIter;
 
   // Receive header.
   while (!Serial1.findUntil(HEADER, '\0')) {
@@ -38,29 +36,15 @@ void loop() {
 
   // Receive body.
   Serial1.readBytesUntil(TERMINATOR[1], sentence, maxSentenceSize);
-  Serial.println(sentence);
-  /*
-  while (true) {
-    readThisIter = 0;
-    toRead = Serial1.available();
-    if (toRead > 0) {
-      if ((toRead + alReady) > maxSentenceSize) {
-        break;  
-      }
-      readThisIter = Serial1.readBytesUntil(TERMINATOR[1], sentence+alReady, toRead);
-      alReady += readThisIter;
-      
-      if (readThisIter < toRead && Serial1.peek() == TERMINATOR[1]) {
-        // Publish complete sentence/packet.
-        Serial1.readBytes(sentence+alReady, 1);
-        Serial.println(sentence);
-        break;
-      }
-    }
-    else {
-      // Serial.println("To read: " + String(toRead));
-      // delay(50);
-    }
+  ++packetsReceived;
+
+  unsigned int currTime = millis();
+  if ((currTime - startTime) >= 1000) {
+    pps = packetsReceived;
+    packetsReceived = 0;
+    startTime = currTime;
   }
-  */
+
+  // Output data and packet rate.
+  Serial.println(sentence+String(pps));
 }
