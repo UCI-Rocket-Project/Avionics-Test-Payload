9a86ca69bba629fb43bbcbca908e5e691193bddb
RSkuma <rsakuma4@gmail.com>
2021-11-09 14:21:00 -0800

    added struct which will replace character strings being used to transmit ATP sensor readings. Not yet tested

--- GroundStation/GroundStation.ino
+++ GroundStation/GroundStation.ino
@@ -1,15 +1,32 @@
-#define HEADER      "Hey,"
-#define TERMINATOR  ",v"
+#define HEADER      "FlaminHotMountainDew"
+#define TERMINATOR  "V"
+
+
+struct DataStructure {
+float GyroX, GyroY, GyroZ;
+float AccX, AccY, AccZ;
+float AngX, AngY, AngZ;
+double Temp, Pres;
+float BMPAlt, Lat, Lng;     
+double GPSAlt, GPSSpeed;    
+uint32_t Satellites;
+} DataPack;
+
 
 const unsigned char maxSentenceSize = 255;
+const unsigned int maxStructSize=sizeof(DataPack);
 
 // Packet buffer variables.
 char sentence[maxSentenceSize+1];
+byte byteArray[maxStructSize];
 
 // Packet diagnostic variables.
 unsigned int pps;
 unsigned int packetsReceived;
 unsigned int startTime;
+
+
+
 
 void setup() {
   Serial.begin(115200);
@@ -19,8 +36,10 @@ void setup() {
   Serial1.setRX(0);
   Serial1.setTX(1);
   Serial1.begin(115200);
+  DataStructure DataReading;
+  int readCount=0;
 
-  sentence[maxSentenceSize] = '\0';
+  sentence[structSize] = '';
   pps = 0;
   packetsReceived = 0;
   startTime = millis();
@@ -28,14 +47,23 @@ void setup() {
 
 void loop() {
   // Reset buffer.
-  memset(sentence, 0, maxSentenceSize);
+  //memset(sentence, 0, maxSentenceSize);
+  memset(byteArray, 0, maxStructSize);
+  memset(DataReading, 0, maxStructSize);
+
 
   // Receive header.
   while (!Serial1.findUntil(HEADER, '\0')) {
   }
 
   // Receive body.
-  Serial1.readBytesUntil(TERMINATOR[1], sentence, maxSentenceSize);
+  //Serial1.readBytesUntil(TERMINATOR[1], sentence, maxSentenceSize);
+  readCount=Serial1.readBytesUntil(TERMINATOR[0], byteArray, maxStructSize);
+
+  if(readCount!=maxStructSize){
+    Serial.println("readCount!=struct size: check if error| Still Currently printing incomplete packets"
+  }
+
   ++packetsReceived;
 
   unsigned int currTime = millis();
@@ -46,5 +74,7 @@ void loop() {
   }
 
   // Output data and packet rate.
-  Serial.println(sentence+String(pps));
+  //Serial.println(sentence+String(pps));
+  Serial.println("Print Recieved Data")
+  
 }
