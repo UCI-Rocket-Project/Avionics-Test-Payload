d6d3e907c32f5438aeabebb974e0949bcd1dbeb8
RSkuma <rsakuma4@gmail.com>
2021-11-10 17:56:22 -0800

    Confirmed packet reading is functional, can read bytes and convert back into data struct. But not working 100% of time

--- GroundStation/GroundStation.ino
+++ GroundStation/GroundStation.ino
@@ -1,33 +1,29 @@
 #define HEADER      "FlaminHotMountainDew"
 #define TERMINATOR  "V"
 
+const unsigned int packetSize=84; //assumes no packing
 
+#pragma pack(push,1)
 struct DataStructure {
-float GyroX, GyroY, GyroZ;
-float AccX, AccY, AccZ;
-float AngX, AngY, AngZ;
-double Temp, Pres;
-float BMPAlt, Lat, Lng;     
-double GPSAlt, GPSSpeed;    
-uint32_t Satellites;
-} DataPack;
-
-
-const unsigned char maxSentenceSize = 255;
-const unsigned int maxStructSize=sizeof(DataPack);
-
-// Packet buffer variables.
-char sentence[maxSentenceSize+1];
-byte byteArray[maxStructSize];
+  float GyroX, GyroY, GyroZ;
+  float AccX, AccY, AccZ;
+  float AngX, AngY, AngZ;
+  double Temp, Pres;
+  float BMPAlt, Lat, Lng;     
+  double GPSAlt, GPSSpeed;    
+  uint32_t Satellites;
+} data;
+#pragma pack(pop)
+char dataBytes[84];
 
 // Packet diagnostic variables.
 unsigned int pps;
 unsigned int packetsReceived;
 unsigned int startTime;
 
-
-
-
+//moved variables outside of loop 
+unsigned int currTime=0;
+int readCount=0;
 void setup() {
   Serial.begin(115200);
 
@@ -36,10 +32,7 @@ void setup() {
   Serial1.setRX(0);
   Serial1.setTX(1);
   Serial1.begin(115200);
-  DataStructure DataReading;
-  int readCount=0;
-
-  sentence[structSize] = '';
+  
   pps = 0;
   packetsReceived = 0;
   startTime = millis();
@@ -48,33 +41,43 @@ void setup() {
 void loop() {
   // Reset buffer.
   //memset(sentence, 0, maxSentenceSize);
-  memset(byteArray, 0, maxStructSize);
-  memset(DataReading, 0, maxStructSize);
-
+  memset(&data, 0, packetSize);
+  
 
   // Receive header.
   while (!Serial1.findUntil(HEADER, '\0')) {
+   // Serial.println("Waiting for start...");
+//    delay(10);
   }
 
   // Receive body.
-  //Serial1.readBytesUntil(TERMINATOR[1], sentence, maxSentenceSize);
-  readCount=Serial1.readBytesUntil(TERMINATOR[0], byteArray, maxStructSize);
-
-  if(readCount!=maxStructSize){
-    Serial.println("readCount!=struct size: check if error| Still Currently printing incomplete packets"
+  readCount = Serial1.readBytesUntil(TERMINATOR[0], (byte*) &data, packetSize);
+  Serial1.flush();
+  if(readCount != packetSize-1){
+    Serial.println("readCount is " + (String)readCount);
+//    Serial.println("readCount!=struct size: check if error| Still Currently printing incomplete packets");
+    return;
   }
 
+  // Packet received.
   ++packetsReceived;
 
-  unsigned int currTime = millis();
+  currTime = millis();
   if ((currTime - startTime) >= 1000) {
     pps = packetsReceived;
     packetsReceived = 0;
     startTime = currTime;
   }
-
+  Serial.println("Printing Data Pack: Size= "+(String)readCount);
   // Output data and packet rate.
-  //Serial.println(sentence+String(pps));
-  Serial.println("Print Recieved Data")
-  
+   
+  Serial.print((String)data.GyroX + ',' + (String)data.GyroY + ',' + (String) data.GyroZ + ',') ;
+  Serial.print((String)data.AccX + ',' + (String)data.AccY + ',' + (String)data.AccZ + ',' );
+  Serial.print((String)data.AngX + ',' + (String)data.AngY + ',' + (String)data.AngZ + ',' );
+  Serial.print((String)data.Temp + ',' + (String)data.Pres + ',');
+  Serial.print((String)data.BMPAlt + ',' + (String)data.Lat + ',' + (String)data.Lng + ',' );
+  Serial.print( (String)data.GPSAlt + ',' + (String)data.GPSSpeed + ',' );
+  Serial.print((String)data.Satellites + ',' + (String)pps );
+Serial.println();
+
 }
