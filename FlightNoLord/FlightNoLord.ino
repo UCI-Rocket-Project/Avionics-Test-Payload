94b641771f8006b102bfc7e556c0e61d1c47afcb
Tyler Cook <tylercook017@gmail.com>
2022-05-09 12:16:17 -0700

    Early struct transmission work. Unchanged since use.

---
+++ FlightNoLord/FlightNoLord.ino
@@ -0,0 +1,142 @@
+/* Useful Resources
+ * MPU6050 Datasheet - https://invensense.tdk.com/wp-content/uploads/2015/02/MPU-6000-Datasheet1.pdf
+ */
+
+#include <Adafruit_Sensor.h>     // sensor abstraction library
+#include <Adafruit_BMP3XX.h>     // barometric pressure sensor
+#include <SD.h>
+#include <TinyMPU6050.h>
+#include <Wire.h>
+
+#define SEALEVELPRESSURE_HPA (1013.2)
+
+MPU6050 mpu(Wire);
+Adafruit_BMP3XX bmp = Adafruit_BMP3XX();
+
+char* telemFileName = "telem.csv";
+char loopIndex = 0;
+
+void setup() {
+  Serial.begin(115200);
+
+  // Setup I2C bus (Used by the NXP 9-DOF & the BMP388 breakout boards)
+  Wire.setSDA(18);
+  Wire.setSCL(19);
+
+  // Setup IMU (MPU6050) using TinyMPU6050 library.
+  mpu.Initialize(); // Must do this after setting up Wire, since it internally calls 
+  mpu.RegisterWrite(MPU6050_ACCEL_CONFIG, 0x08);  // By default, TinyMPU6050 uses +-2g, we want +-16g
+  Serial.println("Starting MPU6050 Calibration...");
+  mpu.Calibrate();
+  Serial.println("MPU6050 Calibration complete.");
+
+  // Wait for the hardware to initialize
+  bool initialized = false;
+  while (!initialized) {
+    delay(1000);
+    initialized = true;
+    if (!bmp.begin_I2C(0x77)) {
+      Serial.println("Error detecting BMP388");
+      initialized = false;
+    }
+    if (!SD.begin(BUILTIN_SDCARD)) {
+      Serial.println("Error detecting SD card");
+      initialized = false;
+    }
+  }
+
+  // Setup Barometer (BMP388)
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_2X); // needed for the bmp388
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_2X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+
+  // Setup XBee UART
+  Serial1.clear();
+  Serial1.setRX(0);
+  Serial1.setTX(1);
+  Serial1.begin(115200);
+
+  // Write to telemetry file, can use as a spacer to know what data is new.
+  writeToFile(telemFileName, "Test");
+
+  delay(500); // Delay for smoothing initial BMP
+  Serial.println("Setup complete.");
+}
+
+void loop() {
+  mpu.Execute();  // Update values (which mpu.Get____() will read from) with new data.
+
+  // Time
+  unsigned long timestamp = millis();
+  Serial.println(timestamp);
+
+  // Gyro x,y,z [degrees/second]
+  Serial.print("\t");
+  Serial.print(mpu.GetGyroX());
+  Serial.print(", ");
+  Serial.print(mpu.GetGyroY());
+  Serial.print(", ");
+  Serial.println(mpu.GetGyroZ());
+
+  // Acceleration x,y,z [m/s²]
+  Serial.print("\t");
+  Serial.print(mpu.GetAccX());
+  Serial.print(", ");
+  Serial.print(mpu.GetAccY());
+  Serial.print(", ");
+  Serial.println(mpu.GetAccZ());
+
+  // Attitude (yaw, pitch, roll in [degrees])
+  Serial.print("\t");
+  Serial.print(mpu.GetAngX());
+  Serial.print(", ");
+  Serial.print(mpu.GetAngY());
+  Serial.print(", ");
+  Serial.println(mpu.GetAngZ());
+
+  // Read bmp tempature [C], pressure [hPa], and altitude [m]
+  Serial.print("\t");
+  Serial.print(bmp.temperature);  
+  Serial.print(", ");
+  Serial.print(bmp.pressure / 100);
+  Serial.print(", ");
+  Serial.println(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+
+  String newTelem = String(timestamp) + ',';
+  newTelem += String(mpu.GetGyroX()) + ',';
+  newTelem += String(mpu.GetGyroY()) + ',';
+  newTelem += String(mpu.GetGyroZ());
+  newTelem += ',';
+  newTelem += String(mpu.GetAccX()) + ',';
+  newTelem += String(mpu.GetAccY()) + ',';
+  newTelem += String(mpu.GetAccZ());
+  newTelem += ',';
+  newTelem += String(mpu.GetAngX()) + ',';
+  newTelem += String(mpu.GetAngY()) + ',';
+  newTelem += String(mpu.GetAngZ());
+  newTelem += ',';
+  newTelem += String(bmp.temperature) + ',';
+  newTelem += String(bmp.pressure) + ',';
+  newTelem += String(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+  writeToFile(telemFileName, newTelem);
+
+  //Send data over XBee once every "2" 10 loops
+  if (loopIndex >= 1) {
+    Serial1.println(newTelem);
+    loopIndex = -1; // Will be updated below to 0
+  }
+  ++loopIndex;
+}
+
+void writeToFile(char* fileName, String telem) {
+  File telemFile = SD.open(fileName, FILE_WRITE);
+  if (telemFile) {
+    telemFile.println(telem);
+    telemFile.close();
+    Serial.println("Wrote to " + (String)fileName + " on SD");
+  }
+  else {
+    Serial.println("Error writing to " + (String)fileName + " on SD");
+  }
+}
--- GroundStation/GroundStation.ino
+++ GroundStation/GroundStation.ino
@@ -1,32 +1,22 @@
 #define HEADER      "FlaminHotMountainDew"
-#define TERMINATOR  "V"
+#define TERMINATOR  "v"
 
 
 struct DataStructure {
-float GyroX, GyroY, GyroZ;
-float AccX, AccY, AccZ;
-float AngX, AngY, AngZ;
-double Temp, Pres;
-float BMPAlt, Lat, Lng;     
-double GPSAlt, GPSSpeed;    
-uint32_t Satellites;
-} DataPack;
-
-
-const unsigned char maxSentenceSize = 255;
-const unsigned int maxStructSize=sizeof(DataPack);
-
-// Packet buffer variables.
-char sentence[maxSentenceSize+1];
-byte byteArray[maxStructSize];
+  float GyroX, GyroY, GyroZ;
+  float AccX, AccY, AccZ;
+  float AngX, AngY, AngZ;
+  double Temp, Pres;
+  float BMPAlt, Lat, Lng;     
+  double GPSAlt, GPSSpeed;    
+  uint32_t Satellites;
+} data;
+char dataBytes[84];
 
 // Packet diagnostic variables.
 unsigned int pps;
 unsigned int packetsReceived;
 unsigned int startTime;
-
-
-
 
 void setup() {
   Serial.begin(115200);
@@ -36,34 +26,34 @@ void setup() {
   Serial1.setRX(0);
   Serial1.setTX(1);
   Serial1.begin(115200);
-  DataStructure DataReading;
-  int readCount=0;
-
-  sentence[structSize] = '';
+  
   pps = 0;
   packetsReceived = 0;
   startTime = millis();
+  Serial.print(sizeof(data));
 }
 
 void loop() {
   // Reset buffer.
   //memset(sentence, 0, maxSentenceSize);
-  memset(byteArray, 0, maxStructSize);
-  memset(DataReading, 0, maxStructSize);
-
+  memset(&data, 0, sizeof(data));
 
   // Receive header.
   while (!Serial1.findUntil(HEADER, '\0')) {
+//    Serial.println("Waiting for start...");
+//    delay(10);
   }
 
   // Receive body.
-  //Serial1.readBytesUntil(TERMINATOR[1], sentence, maxSentenceSize);
-  readCount=Serial1.readBytesUntil(TERMINATOR[0], byteArray, maxStructSize);
+  int readCount = Serial1.readBytesUntil(TERMINATOR[0], (byte*) &data, sizeof(data));
 
-  if(readCount!=maxStructSize){
-    Serial.println("readCount!=struct size: check if error| Still Currently printing incomplete packets"
+  if(readCount != 84){
+//    Serial.println("readCount is " + (String)readCount);
+//    Serial.println("readCount!=struct size: check if error| Still Currently printing incomplete packets");
+    return;
   }
 
+  // Packet received.
   ++packetsReceived;
 
   unsigned int currTime = millis();
@@ -74,7 +64,11 @@ void loop() {
   }
 
   // Output data and packet rate.
-  //Serial.println(sentence+String(pps));
-  Serial.println("Print Recieved Data")
-  
+  Serial.println((String)data.GyroX + ',' + (String)data.GyroY + ',' + (String) data.GyroZ + ',' + 
+    (String)data.AccX + ',' + (String)data.AcbcY + ',' + (String)data.AccZ + ',' + 
+    (String)data.AngX + ',' + (String)data.AngY + ',' + (String)data.AngZ + ',' + 
+    (String)data.Temp + ',' + (String)data.Pres + ',' + 
+    (String)data.BMPAlt + ',' + (String)data.Lat + ',' + (String)data.Lng + ',' + 
+    (String)data.GPSAlt + ',' + (String)data.GPSSpeed + ',' + 
+    (String)data.Satellites + ',' + (String)pps);
 }
